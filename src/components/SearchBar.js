b249ffd5598e6ffb95b6c8c66c49f4fd0e5f69ee
MarcusMellorMIM <marcus@mellor.com>
2019-07-02 11:36:06 +0100

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,24 @@
 import React from 'react';
 
-const SearchBar = () => {
+// 5) Add in the callback function, to set the type
+const SearchBar = (props) => {
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" name="sortradio" value="Alphabetically" checked={null} onChange={props.setOrder}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" name="sortradio" value="Price" checked={null} onChange={props.setOrder}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={props.setTypeState}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,26 @@
 import React from 'react'
+// 4) Render the stock from props
+const Stock = (props) => {
 
-const Stock = () => (
+const {id, ticker,name,price} = props.stock;
+
+// 5) Add a callback to biuy a stock
+return (
   <div>
-
     <div className="card">
       <div className="card-body">
-        <h5 className="card-title">{
+        <h5 className="card-title" onClick={() => props.buyStock(id)}>{
             //Company Name
+            name
           }</h5>
         <p className="card-text">{
             //ticker: stock price
+            ticker + ':' + price
           }</p>
       </div>
     </div>
-
-
-  </div>
-);
+  </div> 
+)
+};
 
 export default Stock
