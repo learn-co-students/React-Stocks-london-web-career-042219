a864b7b63f1d22df30bfda0bb9618e4c30e51482
kwren96 <katherine.wren196@gmail.com>
2019-07-02 14:09:14 +0100

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
-import StockContainer from './StockContainer'
-import PortfolioContainer from './PortfolioContainer'
-import SearchBar from '../components/SearchBar'
+import React, { Component } from "react";
+import StockContainer from "./StockContainer";
+import PortfolioContainer from "./PortfolioContainer";
+import SearchBar from "../components/SearchBar";
 
 class MainContainer extends Component {
-
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar
+          sortFunction={this.props.sortFunction}
+          filterStocks={this.props.filterStocks}
+        />
 
-          <div className="row">
-            <div className="col-8">
-
-              <StockContainer/>
-
-            </div>
-            <div className="col-4">
-
-              <PortfolioContainer/>
-
-            </div>
+        <div className="row">
+          <div className="col-8">
+            <StockContainer
+              stocks={this.props.stocks}
+              editPortfolioStatus={this.props.editPortfolioStatus}
+            />
           </div>
+          <div className="col-4">
+            <PortfolioContainer
+              portfolio={this.props.portfolio}
+              editPortfolioStatus={this.props.editPortfolioStatus}
+            />
+          </div>
+        </div>
       </div>
     );
   }
-
 }
 
 export default MainContainer;
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import Stock from '../components/Stock'
+import React, { Component } from "react";
+import Stock from "../components/Stock";
 
 class StockContainer extends Component {
-
   render() {
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        {this.props.stocks.map(stock => (
+          <Stock
+            stock={stock}
+            editPortfolioStatus={this.props.editPortfolioStatus}
+          />
+        ))}
       </div>
     );
   }
-
 }
 
 export default StockContainer;
