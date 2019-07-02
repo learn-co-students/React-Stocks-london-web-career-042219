ce80758880ae3fc7bcfef6b52abc8a852b753721
Zubair <zubair.maqsood6@gmail.com>
2019-07-02 16:19:32 +0100

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -8,7 +8,7 @@ class PortfolioContainer extends Component {
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            this.props.myPortfolio.map((portfolio, index) => <Stock key={index} stock={portfolio} addToPortfolio={this.props.removeFromPortfolio}/>)
           }
       </div>
     );
